98eab0e7116543e56986860a2e75ed91a68436b8
Shlok Bhakta <shlokbhakta1@gmail.com>
2025-02-10 21:25:49 +0000

    scaffold users table

---
+++ spec/views/users/index.html.tailwindcss_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+RSpec.describe "users/index", type: :view do
+  before(:each) do
+    assign(:users, [
+      User.create!(
+        role: nil,
+        first_name: "First Name",
+        last_name: "Last Name",
+        email: "Email"
+      ),
+      User.create!(
+        role: nil,
+        first_name: "First Name",
+        last_name: "Last Name",
+        email: "Email"
+      )
+    ])
+  end
+
+  it "renders a list of users" do
+    render
+    cell_selector = 'div>p'
+    assert_select cell_selector, text: Regexp.new(nil.to_s), count: 2
+    assert_select cell_selector, text: Regexp.new("First Name".to_s), count: 2
+    assert_select cell_selector, text: Regexp.new("Last Name".to_s), count: 2
+    assert_select cell_selector, text: Regexp.new("Email".to_s), count: 2
+  end
+end
