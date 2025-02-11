46155b55c679dde6743618ab8ef160570410d0dc
Shlok Bhakta <shlokbhakta1@gmail.com>
2025-02-11 00:11:22 +0000

    Revert "setup basic oauth pipeline"
    This reverts commit ab52256912c623e88224e8719e10fba072e91c02.

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,8 +11,6 @@ class User < ApplicationRecord
                    uniqueness: true,
                    format: { with: URI::MailTo::EMAIL_REGEXP, message: "must be a valid email address" }
 
-  has_one :identity
-
   def name
     "#{first_name} #{last_name}"
   end
