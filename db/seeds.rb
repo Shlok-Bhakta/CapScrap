d2e1107b5aeace4870c6ae618e213120d1594f36
Sean O'Connor <soc1641@icloud.com>
2025-02-11 01:10:16 +0000

    developed some simple seeding for category, roles, and items

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,32 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+
+Item.delete_all #clears all exisiting data from tables
+
+
+
+category1 = Category.find_or_create_by(name: "Tool") #creating item categories
+category2 = Category.find_or_create_by(name: "Material")
+
+role1 = Role.find_or_create_by(name: "user") #creating roles
+role2 = Role.find_or_create_by(name: "admin")
+
+Item.create!([ #creates items
+  { category: category1, description: "VR Headset", location: "HRBB" },
+  { category: category1, description: "VR Headset", location: "ILCB" },
+  { category: category1, description: "Mouse", location: "HELD" },
+  { category: category1, description: "Headphones", location: "ZACH" },
+  { category: category1, description: "Mouse", location: "Zach" },
+  { category: category1, description: "HDMI Cable", location: "ZACH" },
+  { category: category2, description: "Styrofoam", location: "HRBB" },
+  { category: category2, description: "Plutonium", location: "ILCB" },
+  { category: category2, description: "Wooden Plank", location: "HELD" },
+  { category: category2, description: "Sheet Metal", location: "ZACH" },
+  { category: category2, description: "Styrofoam", location: "Zach" },
+  { category: category2, description: "Glass", location: "ZACH" },
+])
+
+
+
