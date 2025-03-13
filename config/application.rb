0beff2e27721ab12e79065de21ba8ceda05df307
Sean O'Connor <soc1641@icloud.com>
2025-03-13 02:32:57 +0000

    created logger and file to store log info

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,7 @@
 require_relative "boot"
 
 require "rails/all"
+require "logger"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -9,7 +10,7 @@ Bundler.require(*Rails.groups)
 module Capscrap
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 8.0
+    config.load_defaults 7.0
 
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
@@ -23,5 +24,20 @@ module Capscrap
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+
+    # Set up a rotating logger with a max file size of 10MB, keeping only 5 rotated files
+    log_file = Rails.root.join("log", "application.log")
+    logger = Logger.new(log_file, 5, 10 * 1024 * 1024) # 5 files, each max 10MB
+    # Customize log format
+    logger.formatter = proc do |severity, datetime, progname, msg|
+      "[#{datetime.strftime('%Y-%m-%d %H:%M:%S')}] #{severity} #{progname}: #{msg}\n"
+    end
+    # Enable tagged logging
+    config.logger = ActiveSupport::TaggedLogging.new(logger)
+    # Set logging level (INFO recommended for production)
+    config.logger.level = Logger::INFO
+
+
   end
 end
