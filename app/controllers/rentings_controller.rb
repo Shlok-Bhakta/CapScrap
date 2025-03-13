dd59828354c13c78bb9f2bee46d97bbba8bdbf84
Sean O'Connor <soc1641@icloud.com>
2025-03-13 02:37:27 +0000

    added logging for renting controller

--- app/controllers/rentings_controller.rb
+++ app/controllers/rentings_controller.rb
@@ -3,6 +3,7 @@ class RentingsController < ApplicationController
 
   # GET /rentings or /rentings.json
   def index
+    logger.debug "#{current_user.email} IS VIEWING THE RENTING PAGE:"
     if params[:sort].present?
       @rentings = Renting.order(params[:sort] => :desc)
     else
@@ -12,15 +13,18 @@ class RentingsController < ApplicationController
 
   # GET /rentings/1 or /rentings/1.json
   def show
+    logger.debug "#{current_user.email} IS VIEWING THE RENTING INFORMATION: #{@renting.attributes.inspect}"
   end
 
   # GET /rentings/new
   def new
     @renting = Renting.new
+    logger.debug "#{current_user.email} IS VIEWING THE NEW RENTING INFORMATION: #{@renting.attributes.inspect}"
   end
 
   # GET /rentings/1/edit
   def edit
+    logger.debug "#{current_user.email}  IS EDITING THE RENTING INFORMATION: #{@renting.attributes.inspect}"
   end
 
   # POST /rentings or /rentings.json
@@ -31,6 +35,7 @@ class RentingsController < ApplicationController
       if @renting.save
         format.html { redirect_to @renting, notice: "Renting was successfully created." }
         format.json { render :show, status: :created, location: @renting }
+        logger.debug "#{current_user.email} IS CREATING THE RENTING INFORMATION: #{@renting.attributes.inspect}"
       else
         format.html { render :new, status: :unprocessable_entity }
         format.json { render json: @renting.errors, status: :unprocessable_entity }
@@ -44,6 +49,7 @@ class RentingsController < ApplicationController
       if @renting.update(renting_params)
         format.html { redirect_to @renting, notice: "Renting was successfully updated." }
         format.json { render :show, status: :ok, location: @renting }
+        logger.debug "#{current_user.email}  IS UPDATING THE RENTING INFORMATION: #{@renting.attributes.inspect}"
       else
         format.html { render :edit, status: :unprocessable_entity }
         format.json { render json: @renting.errors, status: :unprocessable_entity }
@@ -54,6 +60,7 @@ class RentingsController < ApplicationController
   # DELETE /rentings/1 or /rentings/1.json
   def destroy
     @renting.destroy!
+    logger.debug "#{current_user.email} IS DELETING THE RENTING INFORMATION: #{@renting.attributes.inspect}"
 
     respond_to do |format|
       format.html { redirect_to rentings_path, status: :see_other, notice: "Renting was successfully destroyed." }
