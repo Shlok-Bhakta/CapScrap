692c1308aa34848de8c80f912443e107bdc33afe
Sean O'Connor <soc1641@icloud.com>
2025-03-15 00:57:42 +0000

    fixed item logging to work if current_user is null

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,7 +3,7 @@ class ItemsController < ApplicationController
 
   # GET /items or /items.json
   def index
-    logger.debug "#{current_user.email} IS VIEWING THE ITEMS PAGE:"
+    logger.debug "#{current_user  ? current_user.email : 'GUEST'} IS VIEWING THE ITEMS PAGE:"
     if params[:query].present?
       # Search by ID if the input is numeric; otherwise, search by name
       @items = params[:query].match?(/^\d+$/) ? Item.where(id: params[:query]) : Item.where("description ILIKE ?", "%#{params[:query]}%")
@@ -16,18 +16,18 @@ class ItemsController < ApplicationController
 
   # GET /items/1 or /items/1.json
   def show
-     logger.debug "#{current_user.email} IS VIEWING THE ITEM INFORMATION: #{@item.attributes.inspect}"
+     logger.debug "#{current_user  ? current_user.email : 'GUEST'} IS VIEWING THE ITEM INFORMATION: #{@item.attributes.inspect}"
   end
 
   # GET /items/new
   def new
     @item = Item.new
-    logger.debug "#{current_user.email} IS VIEWING THE NEW ITEM INFORMATION: #{@item.attributes.inspect}"
+    logger.debug "#{current_user  ? current_user.email : 'GUEST'} IS VIEWING THE NEW ITEM INFORMATION: #{@item.attributes.inspect}"
   end
 
   # GET /items/1/edit
   def edit
-    logger.debug "#{current_user.email} IS EDITING THE ITEM INFORMATION: #{@item.attributes.inspect}"
+    logger.debug "#{current_user  ? current_user.email : 'GUEST'} IS EDITING THE ITEM INFORMATION: #{@item.attributes.inspect}"
   end
 
   # POST /items or /items.json
@@ -38,7 +38,7 @@ class ItemsController < ApplicationController
       if @item.save
         format.html { redirect_to @item, notice: "Item was successfully created." }
         format.json { render :show, status: :created, location: @item }
-        logger.debug "#{current_user.email} IS CREATING THE ITEM INFORMATION: #{@item.attributes.inspect}"
+        logger.debug "#{current_user  ? current_user.email : 'GUEST'} IS CREATING THE ITEM INFORMATION: #{@item.attributes.inspect}"
       else
         format.html { render :new, status: :unprocessable_entity }
         format.json { render json: @item.errors, status: :unprocessable_entity }
@@ -52,7 +52,7 @@ class ItemsController < ApplicationController
       if @item.update(item_params)
         format.html { redirect_to @item, notice: "Item was successfully updated." }
         format.json { render :show, status: :ok, location: @item }
-        logger.debug "#{current_user.email} IS UPDATING THE ITEM INFORMATION: #{@item.attributes.inspect}"
+        logger.debug "#{current_user  ? current_user.email : 'GUEST'} IS UPDATING THE ITEM INFORMATION: #{@item.attributes.inspect}"
       else
         format.html { render :edit, status: :unprocessable_entity }
         format.json { render json: @item.errors, status: :unprocessable_entity }
@@ -63,7 +63,7 @@ class ItemsController < ApplicationController
   # DELETE /items/1 or /items/1.json
   def destroy
     @item.destroy!
-    logger.debug "#{current_user.email} IS DELETING THE ITEM INFORMATION: #{@item.attributes.inspect}"
+    logger.debug "#{current_user  ? current_user.email : 'GUEST'} IS DELETING THE ITEM INFORMATION: #{@item.attributes.inspect}"
 
     respond_to do |format|
       format.html { redirect_to items_path, status: :see_other, notice: "Item was successfully destroyed." }
