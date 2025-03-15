8057319463987120a87b27659693c1ae20a36c7d
Shlok Bhakta <shlokbhakta1@gmail.com>
2025-03-15 07:06:55 +0000

    fix bug with login page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,15 @@
 module ApplicationHelper
   include Pagy::Frontend
+
+  def devise_mapping
+    @devise_mapping ||= Devise.mappings[:user]
+  end
+
+  def resource_name
+    devise_mapping.name
+  end
+
+  def resource_class
+    devise_mapping.to
+  end
 end
