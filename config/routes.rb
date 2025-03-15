2fd5c9e72af01335de875a9f08e3acff126f8c6a
Faris <faris.ahmed@tamu.edu>
2025-03-15 23:51:51 +0000

    Added Help Page and Help Button in Navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get "pages/help"
   namespace :admin do
     get "dashboard/users"
     get "dashboard/items"
@@ -20,7 +21,8 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
+  # Can be used by load balancers and uptime monitors to verify that the app is live. 
+  get "/help", to: "pages#help"  # New route for Help page
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
