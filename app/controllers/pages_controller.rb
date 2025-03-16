bd12243161e208b5980e744097195a0448638adf
Faris <faris.ahmed@tamu.edu>
2025-03-16 06:16:47 +0000

    Fixed lint test again

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:help] # Allow everyone access to help page
+  skip_before_action :authenticate_user!, only: [ :help ] # Allow everyone access to help page
 
   def help
   end
