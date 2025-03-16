f91967516a4b55eee60e02d907ecbc3afd772b85
Shlok Bhakta <shlokbhakta1@gmail.com>
2025-03-16 02:08:50 +0000

    add options for search to controller

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -27,6 +27,71 @@ class Admin::DashboardController < ApplicationController
   end
 
   def items
+    @items_query = Item.search(params[:query])
+                      .sort_by_field(params[:sort], params[:direction])
+                      .joins(:category)
+    @pagy, @items = pagy(@items_query, items: params[:per_page] || 50)
+    @categories = Category.all
+
+    respond_to do |format|
+      format.html
+      format.turbo_stream {
+        render turbo_stream: turbo_stream.replace(
+          "items_table",
+          partial: "admin/dashboard/items_table",
+          locals: {
+            items: @items,
+            pagy: @pagy,
+            categories: @categories,
+            sort_field: params[:sort],
+            sort_direction: params[:direction]
+          }
+        )
+      }
+    end
+  end
+
+  def create_item
+    @item = Item.new(item_params)
+    if @item.save
+      render json: { success: true, message: "Item created successfully" }
+    else
+      render json: {
+        success: false,
+        error: @item.errors.full_messages.join(", ")
+      }, status: :unprocessable_entity
+    end
+  end
+
+  def update_item
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      render json: { success: true, message: "Item updated successfully" }
+    else
+      render json: {
+        success: false,
+        error: @item.errors.full_messages.join(", ")
+      }, status: :unprocessable_entity
+    end
+  end
+
+  def delete_item
+    @item = Item.find(params[:id])
+    if @item.destroy
+      render json: { success: true, message: "Item deleted successfully" }
+    else
+      render json: {
+        success: false,
+        error: "Failed to delete item"
+      }, status: :unprocessable_entity
+    end
+  end
+
+
+  def renting
+  end
+
+  def purchased
   end
 
   def update_user_role
@@ -45,5 +110,9 @@ class Admin::DashboardController < ApplicationController
       flash[:alert] = "You are not authorized to access this area"
       redirect_to root_path
     end
+  end
+
+  def item_params
+    params.require(:item).permit(:description, :location, :category_id)
   end
 end
